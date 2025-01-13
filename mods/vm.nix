8458e7d64ddd6ef412bb8c47d49eb10494e7df69
flexxfluxx <felix@vonharrach.de>
2025-01-13 16:06:45 +0100

    fucking whatever

--- mods/vm.nix
+++ mods/vm.nix
@@ -4,23 +4,25 @@ let
 in {
     options.mods.vm = {
         enable = lib.mkEnableOption "enable kernel opts to allow gpu passthrough to vm";
-        cpu = lib.mkOption {
-            description = "which cpu vendor, bc apparently that matters";
-            default = "amd";
-        };
     };
     config = lib.mkIf cfg.enable {
-        boot.kernelParams = [
-            "${cfg.cpu}_iommu=on"
-            # "kvm.ignore_msrs=1"
-            # "video=efifb:off"
-            # "vfio-pci.ids=1002:73ef,1002:ab28"
-        ];
-        environment.systemPackages = with pkgs; [
-            qemu_kvm
-            libvirt
-            virt-manager
-            iproute2
-        ];
+        virtualisation.libvirtd = {
+            enable = true;
+            qemu = {
+                package = pkgs.qemu_kvm;
+                runAsRoot = true;
+                swtpm.enable = true;
+                ovmf = {
+                    enable = true;
+                    packages = [
+                        (pkgs.OVMF.override {
+                            secureBoot = true;
+                            tpmSupport = true;
+                        }).fd
+                    ];
+                };
+            };
+};
+
     };
 }
