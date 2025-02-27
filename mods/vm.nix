f1e0f585cf8e08f8dbd54534c55fec4bdf184afe
flexxfluxx <felix@vonharrach.de>
2025-02-27 18:01:37 +0100

    modprobes for macos vm

--- mods/vm.nix
+++ mods/vm.nix
@@ -35,7 +35,7 @@ in {
             nettools
         ];
 
-        bood.extraModprobeConfig = ''
+        boot.extraModprobeConfig = ''
             options kvm_amd nested=1
             options kvm ignore_msrs=1 report_ignored_msrs=0
         '';
