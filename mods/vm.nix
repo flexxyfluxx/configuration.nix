42677110bdf4d078e3693135bdbc3a3529537024
flexxfluxx <felix@vonharrach.de>
2025-10-12 04:55:33 +0200

    update flake

--- mods/vm.nix
+++ mods/vm.nix
@@ -17,15 +17,15 @@ in {
                 package = pkgs.qemu_kvm;
                 runAsRoot = true;
                 swtpm.enable = true;
-                ovmf = {
-                    enable = true;
-                    packages = [
-                        (pkgs.OVMF.override {
-                            secureBoot = true;
-                            tpmSupport = true;
-                        }).fd
-                    ];
-                };
+                # ovmf = {
+                #     enable = true;
+                #     packages = [
+                #         (pkgs.OVMF.override {
+                #             secureBoot = true;
+                #             tpmSupport = true;
+                #         }).fd
+                #     ];
+                # };
             };
         };
         programs.virt-manager.enable = true;
