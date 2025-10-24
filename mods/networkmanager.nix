51c7b6802b6f29f6ab97cad9cb3c7497206a8f12
flexxyfluxx <felix@vonharrach.de>
2025-10-24 15:06:40 +0200

    add openconnect for vpn support

--- mods/networkmanager.nix
+++ mods/networkmanager.nix
@@ -1,10 +1,13 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 {
     options.mods = {
         networkmanager.enable = lib.mkEnableOption "enable networking";
     };
     config = lib.mkIf config.mods.networkmanager.enable {
-        networking.networkmanager.enable = true;
+        networking.networkmanager = {
+            enable = true;
+            plugins = with pkgs; [ networkmanager-openconnect ];
+        };
         programs.nm-applet.enable = true;
     };
 }
